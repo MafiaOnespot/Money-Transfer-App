9255b6dac9833aba6a2cff1357d6511d744096d1
Somnath Pati <106141387+MafiaOnespot@users.noreply.github.com>
2023-09-08 10:31:39 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React, { Fragment } from "react";
 
 const Header = props => (
   <Fragment>
-    <h1 className="mainTitle">Money transfer app</h1>
-    <h2 className="description">Send fake money to fake people.</h2>
+    <h1 className="mainTitle">Money Transfer App</h1>
+    <h2 className="description">Send Fake Money To Fake People.</h2>
     <h1 className="balance">
-      Balance: <span className="balance__number">€ {props.balance} </span>
+      Balance: <span className="balance__number">₹ {props.balance} </span>
     </h1>
   </Fragment>
 );
