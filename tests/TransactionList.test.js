82929bbc28b067793fe442c74ade925d07b0babb
Somnath Pati <106141387+MafiaOnespot@users.noreply.github.com>
2023-09-08 10:30:18 +0530

    Update TransactionList.test.js

--- tests/TransactionList.test.js
+++ tests/TransactionList.test.js
@@ -6,36 +6,36 @@ const transactions = [
   {
     isModeSend: true,
     executed: true,
-    selectedName: "Mark",
+    selectedName: "",
     amount: 10,
-    newBalance: 292,
+    newBalance: 990,
     date: "Feb 7",
     note: "lunch"
   },
   {
     isModeSend: false,
     executed: true,
-    selectedName: "Lucy",
-    amount: 12,
-    newBalance: 302,
+    selectedName: "",
+    amount: 10,
+    newBalance: 990,
     date: "Feb 2",
     note: ""
   },
   {
     isModeSend: true,
     executed: true,
-    selectedName: "Luke",
-    amount: 25,
-    newBalance: 302,
+    selectedName: "",
+    amount: 10,
+    newBalance: 990,
     date: "Jan 20",
     note: "cab share"
   },
   {
     isModeSend: false,
     executed: true,
-    selectedName: "Josh",
+    selectedName: "",
     amount: 10,
-    newBalance: 327,
+    newBalance: 990,
     date: "Jan 18",
     note: "cinema"
   }
